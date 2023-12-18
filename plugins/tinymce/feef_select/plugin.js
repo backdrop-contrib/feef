558d8391c71b0814a52ae6c270679754206c7507
indigoxela <user@olga.indigofloat.at>
2023-12-18 13:27:48 +0100

    Issue #7: Switch TinyMCE plugin to IIFE

--- plugins/tinymce/feef_select/plugin.js
+++ plugins/tinymce/feef_select/plugin.js
@@ -2,18 +2,22 @@
  * @file
  * TinyMCE File Entity Embed plugin.
  */
-"use strict";
+(function () {
 
-tinymce.PluginManager.add('feef_select', function (editor, url) {
-  editor.ui.registry.addButton('feefselect', {
-    icon: 'feef',
-    tooltip: 'Select file entities to embed',
-    onAction: function () {
-      let dialogUrl = editor.options.get('feefDialogUrl');
-      let saveCallback = function (returnValue) {
-        editor.insertContent(returnValue);
+  'use strict';
+
+  tinymce.PluginManager.add('feef_select', function (editor, url) {
+    editor.ui.registry.addButton('feefselect', {
+      icon: 'feef',
+      tooltip: 'Select file entities to embed',
+      onAction: function () {
+        let dialogUrl = editor.options.get('feefDialogUrl');
+        let saveCallback = function (returnValue) {
+          editor.insertContent(returnValue);
+        }
+        Backdrop.tinymce.openDialog(editor, dialogUrl, null, saveCallback, {});
       }
-      Backdrop.tinymce.openDialog(editor, dialogUrl, null, saveCallback, {});
-    }
+    });
   });
-});
+
+})();
